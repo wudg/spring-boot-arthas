28f6c90a64fab47b3c23595b141c0192a12f4b9a
wudg <321073229@qq.com>
2022-07-08 14:07:53 +0800

    init

---
+++ src/test/java/com/wudiguang/arthas/ArthasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wudiguang.arthas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
